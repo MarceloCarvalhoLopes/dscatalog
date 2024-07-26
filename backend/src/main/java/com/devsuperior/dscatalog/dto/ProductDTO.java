09ac755ba97eede5a1e1dbd938a061cf3cf0ab20
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2024-07-26 18:00:50 -0300

    Product insert, update and delete

--- backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
@@ -14,6 +14,7 @@ public class ProductDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Long id;
+    private String name;
     private String description;
     private Double price;
     private String imgUrl;
@@ -24,8 +25,9 @@ public class ProductDTO implements Serializable {
     public ProductDTO() {
     }
 
-    public ProductDTO(Long id, String description, Double price, String imgUrl, Instant date) {
+    public ProductDTO(Long id, String name, String description, Double price, String imgUrl, Instant date) {
         this.id = id;
+        this.name = name;
         this.description = description;
         this.price = price;
         this.imgUrl = imgUrl;
@@ -34,6 +36,7 @@ public class ProductDTO implements Serializable {
 
     public ProductDTO(Product entity) {
         this.id = entity.getId();
+        this.name = entity.getName();
         this.description = entity.getDescription();
         this.price = entity.getPrice();
         this.imgUrl = entity.getImgUrl();
@@ -51,6 +54,14 @@ public class ProductDTO implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getDescription() {
