659798cc59b21f46a5eef1c7b69ae216c81e151e
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2024-08-15 15:34:54 -0300

    Basic beans validation

--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
@@ -1,6 +1,8 @@
 package com.devsuperior.dscatalog.dto;
 
 import com.devsuperior.dscatalog.entities.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 import java.io.Serializable;
 import java.util.HashSet;
@@ -10,8 +12,12 @@ import java.util.Set;
 public class UserDTO implements Serializable {
 
     private Long id;
+
+    @NotBlank(message = "Campo obrigatório")
     private String firstName;
     private String lastName;
+
+    @Email(message = "Favor digitar um e-mail válido")
     private String email;
 
 
